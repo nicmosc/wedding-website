ffba1434a85317ea3d9b7f8b56af01d123b08d9b
nicmosc <nicmosc@users.noreply.github.com>
2020-02-01 17:06:20 +0100

    move sections to own folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,72 +3,19 @@ import { css } from 'emotion';
 import React from 'react';
 
 import BurgerMenu from '../components/BurgerMenu';
-import NextSectionTrigger from '../components/NextSectionTrigger';
-import Subtitle from '../components/Subtitle';
-import Title from '../components/Title';
-import leftFlower from '../images/intro-left-flower.png';
-import rightFlower from '../images/intro-right-flower.png';
+import { Intro } from '../components/HomeSections';
 import sv from '../utils/vars';
 
 const styles = {
   pageWrapper: css`
     position: relative;
   `,
-  intro: css`
-    position: relative;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `,
-  title: css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  `,
-  rightFlower: css`
-    position: absolute;
-    right: calc(${sv.marginLarge} * -2);
-    bottom: 0;
-    z-index: 99;
-    height: 70vh;
-    display: flex;
-    justify-content: flex-end;
-    pointer-events: none;
 
-    > img {
-      height: 100%;
-      object-fit: contain;
-      transform: scaleX(-1);
-    }
-  `,
-  leftFlower: css`
-    position: absolute;
-    top: calc(${sv.marginLarge} * -2);
-    left: calc(${sv.marginLarge} * -1);
-    z-index: 99;
-    height: 60vh;
-    display: flex;
-    justify-content: flex-start;
-    pointer-events: none;
-
-    > img {
-      height: 100%;
-      object-fit: contain;
-    }
-  `,
   menu: css`
     position: fixed;
     top: ${sv.marginLarge};
     right: ${sv.marginLarge};
     z-index: 9;
-  `,
-  nextSection: css`
-    position: absolute;
-    bottom: ${sv.marginLarge};
-    left: 20%;
-    z-index: 999;
   `,
 };
 
@@ -81,21 +28,7 @@ const Index = () => {
       <FullPage>
         <FullPageSections>
           <FullpageSection style={{ height: '100vh' }}>
-            <div className={styles.intro}>
-              <div className={styles.leftFlower}>
-                <img src={leftFlower} />
-              </div>
-              <div className={styles.title}>
-                <Title>Jemima & Nicolaos</Title>
-                <Subtitle>Are getting married</Subtitle>
-              </div>
-              <div className={styles.rightFlower}>
-                <img src={rightFlower} />
-              </div>
-              <div className={styles.nextSection}>
-                <NextSectionTrigger>When?</NextSectionTrigger>
-              </div>
-            </div>
+            <Intro />
           </FullpageSection>
           <FullpageSection style={{ height: '100vh' }}>Countdown</FullpageSection>
           <FullpageSection style={{ height: '100vh' }}>Location</FullpageSection>
