2bea91f7abde9c9aeaba175a5442cd3076078db9
nicmosc <nicmosc@users.noreply.github.com>
2020-02-09 16:08:50 +0100

    copy cname when building

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,13 @@ module.exports = {
         autoLabel: true,
       },
     },
+    {
+      resolve: 'gatsby-plugin-copy-files',
+      options: {
+        source: `${__dirname}/CNAME`,
+        destination: '',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
