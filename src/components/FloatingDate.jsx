8cf3da28c5193d55d36c60f8b4ef48fa545337af
nicmosc <nicmosc@users.noreply.github.com>
2021-04-26 08:05:13 +0200

    fix side date

--- src/components/FloatingDate.jsx
+++ src/components/FloatingDate.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Subtitle from './Subtitle';
 
 const FloatingDate = () => {
-  return <Subtitle noMargin>11 07 2020</Subtitle>;
+  return <Subtitle noMargin>24 07 2021</Subtitle>;
 };
 
 export default FloatingDate;
